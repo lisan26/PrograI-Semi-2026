28e3eb47883bb3b924c4ab42a4685134a9df871d
Víctor Rodríguez <lisandro1975sv@gmail.com>
2026-09-16 10:33:40 -0600

    Creacion de la base de datos y las tablas alumnos, matriculas y materias

--- miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
@@ -8,12 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace miPrimeaAplicacion
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace miPrimeaAplicacion {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
         }
     }
